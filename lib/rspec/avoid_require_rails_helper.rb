92ee1156b03517a68a39aee54d990e252b7e0442
Erick Sasse <148989+esasse@users.noreply.github.com>
2020-05-20 15:58:39 -0300

    Adds RuboCop config and fix styles

--- lib/rspec/avoid_require_rails_helper.rb
+++ lib/rspec/avoid_require_rails_helper.rb
@@ -4,11 +4,11 @@ module RuboCop
   module Cop
     module RSpec
       class AvoidRequireRailsHelper < Cop
-        MSG = 'Don\'t require rails_helper in your spec. It should be in .rspec file'
+        MSG = "Don't require rails_helper in your spec. It should be in .rspec file"
 
         def on_send(send_node)
           return unless send_node.method?(:require)
-          return unless send_node.arguments[0].value == 'rails_helper'
+          return unless send_node.arguments[0].value == "rails_helper"
 
           add_offense(send_node)
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require "bundler/setup"
-require 'rubocop'
+require "rubocop"
 require "rubocop-custom"
-require 'rubocop/rspec/support'
+require "rubocop/rspec/support"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
